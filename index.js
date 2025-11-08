0de1001fef37d0d30179d692cc20a94d7e47cf19
Omonori <a.hafiz5000@gmail.com>
2025-11-08 20:48:46 +0100

    final commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let counter = 0; //global variable to store the count
+
+function increment() {
+    counter++;
+    document.getElementById("counter").innerText = counter;
+}
+
+function decrement() {
+    counter--;
+    document.getElementById("counter").innerText = counter;
+}
+
+function save() {document.getElementById("counter").innerText = counter;
+    alert("Counter value saved, your current count is " + counter);
+    counter = 0;
+    document.getElementById("counter").innerText = counter;``
+}
+
